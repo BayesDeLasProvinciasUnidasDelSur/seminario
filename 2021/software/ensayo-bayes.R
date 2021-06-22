da430c2bd86f9817d0ad196c86c196c94e075fbf
TobiasCarreira <tobiascarreira@gmail.com>
2021-06-22 17:57:42 -0300

    Script R con graficos de beta, binomial y el experimento de la mesa

---
+++ 2021/software/ensayo-bayes.R
@@ -0,0 +1,25 @@
+# install.package("manipulate")
+# install.package("shiny")
+# install.package("ggplot2")
+library(manipulate)
+dx = 0.01
+x = seq(0, 1, dx)
+
+f = function(k,n,p) {
+  par(mfrow=c(1,2))
+  binom <- dbinom(0:n, n, p)
+  names(binom) <- 0:n
+  cols <- rep('grey', n+1)
+  cols[k+1] = 'red'
+  barplot(binom, col=cols)
+  plot(x, dbeta(x, k+1, n-k+1), type = "l")
+  abline(v=p, col='red')
+}
+
+manipulate(f(k,n,p),
+           k=slider(0, 50, initial=5),
+           n=slider(0, 50, initial=10),
+           p=slider(0,1,initial=.5,step=.05))
+
+# library('shiny')
+# shiny::runGitHub('IS606', 'jbryer', subdir='inst/shiny/BayesBilliards')
